4fb504cc06a299a4ce33075724b4e436b5d2d367
mukhammadaliodiljonov <odiljonovmukhammadali@gmail.com>
2026-01-29 12:01:51 +0100

    School-Project-Spring

---
+++ src/main/java/com/eazybytes/eazyschool/model/Holiday.java
@@ -0,0 +1,30 @@
+package com.eazybytes.eazyschool.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Setter
+@Getter
+@Entity
+@Table(name = "holidays")
+public class Holiday extends BaseEntity{
+
+    @Id
+    private  String day;
+    private  String reason;
+    @Enumerated(EnumType.STRING)
+    private  Type type;
+
+
+    public enum Type {
+        FESTIVAL,FEDERAL
+    }
+
+
+
+
+}
